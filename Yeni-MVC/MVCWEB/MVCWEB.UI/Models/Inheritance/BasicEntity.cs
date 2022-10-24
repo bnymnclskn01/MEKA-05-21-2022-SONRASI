a12e06dc342bd1892dc595ad2fc947e398d4e4fc
360MEKA <bnymncliskn1903@gmail.com>
2022-10-24 22:46:45 +0300

    asd

---
+++ Yeni-MVC/MVCWEB/MVCWEB.UI/Models/Inheritance/BasicEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCWEB.UI.Models.Inheritance
+{
+    public class BasicEntity
+    {
+        public BasicEntity()
+        {
+            this.ID = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid ID { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+        [Required]
+        public DateTime CreateDate { get; set; }
+        [Required]
+        public DateTime LastDate { get; set; }
+    }
+}
